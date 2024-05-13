310b389f3ac03c5d505ff2d36cf11133b7c30fc7
Nick <njaginicholas@gmail.com>
2024-05-13 19:45:07 +0300

    update Months.tsx

--- src/components/Months.tsx
+++ src/components/Months.tsx
@@ -1,10 +1,12 @@
-import { months } from "../utils"         
+import { months } from "../utils"    
+import { useAppSelector } from "../hooks"     
 
 const Months = () => {
+  const {currentMonth} = useAppSelector(state => state.date)
   return (
     <div className="grid grid-cols-6 border border-black w-full max-w-2xl">
       {months.map((month, index) => {
-        return <span key={index} className={`grid place-content-center border border-black py-1 text-[12px] tracking-widest ${new Date().getMonth() === index ? 'bg-[#B4B0A5]' : ''}`}>{month.slice(0, 3)}</span>
+        return <span key={index} className={`grid place-content-center border border-black py-1 text-[12px] tracking-widest ${currentMonth === index ? 'bg-[#B4B0A5]' : ''}`}>{month.slice(0, 3)}</span>
       })}
     </div>
   )
