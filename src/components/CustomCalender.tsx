c031925b8523ec7220c6e497f6d08cac897665b5
Nick <njaginicholas@gmail.com>
2024-05-13 19:43:22 +0300

    update CustomCalender.tsx

--- src/components/CustomCalender.tsx
+++ src/components/CustomCalender.tsx
@@ -1,17 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { next, prev } from '../dates/dateSlice';
+import { useAppDispatch, useAppSelector } from '../hooks';
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function CustomCalender() {
-  const [value, onChange] = useState<Value>(new Date());
+  const {currentMonth} = useAppSelector(state => state.date)
+  const [value, setValue] = useState<Value>(new Date(`2024, ${currentMonth + 1}, ${new Date().getDate()}`));
+  const dispatch = useAppDispatch()
+
+  useEffect(()=>{
+    setValue(new Date(`2024, ${currentMonth + 1}`))
+  },[currentMonth])
 
   return (
-    <div className="md:order-1">
+    <div className="md:order-1 ">
       <h2 className="text-center font-semibold tracking-[3px]">calender</h2>
-      <Calendar onChange={onChange} value={value} />
+      <div className="flex items-center gap-2 w-fit mx-auto">
+        <button onClick={() => dispatch(prev())} className="">
+          <ChevronLeft />
+        </button>
+        <Calendar value={value} />
+        <button onClick={() => dispatch(next())} className="">
+          <ChevronRight />
+        </button>
+      </div>
     </div>
   );
 }
