c34f768f53af286f1f3b9e71799818501c16d1e9
Nick <njaginicholas@gmail.com>
2024-05-13 19:42:42 +0300

    update src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
+import {store} from './store'
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
