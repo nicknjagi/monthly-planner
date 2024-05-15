277d7d233ba7079e4ac313e6c199e95ba286fc88
Nick <njaginicholas@gmail.com>
2024-05-15 14:44:53 +0300

    update InstallButton.tsx

--- src/components/InstallButton.tsx
+++ src/components/InstallButton.tsx
@@ -13,7 +13,11 @@ interface BeforeInstallPromptEvent extends Event {
 const InstallButton: React.FC = () => {
   const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(null);
   // State to control the visibility of the Install button
-  const [showInstallButton, setShowInstallButton] = useState<boolean>(() =>!window.matchMedia('(display-mode: standalone)').matches);
+  const [showInstallButton, setShowInstallButton] = useState<boolean>(() =>{
+    // Check if the app has been installed before
+    const isAppInstalled = localStorage.getItem('appInstalled') === 'true';
+    return !isAppInstalled && !window.matchMedia('(display-mode: standalone)').matches;
+  });
 
 
   useEffect(() => {
@@ -25,11 +29,25 @@ const InstallButton: React.FC = () => {
       // Show the Install button when the BeforeInstallPromptEvent is fired
       setShowInstallButton(true);
     };
-  
+    
     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-  
+    
     return () => {
       window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    };
+  }, []);
+  
+  useEffect(() => {
+    const handleAppInstalled = () => {
+      console.log('App installed successfully.');
+      localStorage.setItem('appInstalled', 'true');
+      setShowInstallButton(false);
+    };
+
+    window.addEventListener('appinstalled', handleAppInstalled);
+
+    return () => {
+      window.removeEventListener('appinstalled', handleAppInstalled);
     };
   }, []);
 
@@ -51,7 +69,7 @@ const InstallButton: React.FC = () => {
   return (
     <div className='flex justify-end absolute top-0 right-0'>
       {showInstallButton  && (
-          <button className="flex gap-1 items-center px-4 py-1 rounded-full bg-forrest-green hover:bg-opacity-90 text-white m-2" onClick={installApp}>
+          <button className="flex gap-1 items-center px-4 py-2 rounded-full bg-forrest-green hover:bg-opacity-90 transition text-white m-2 shadow-md shadow-green-50" onClick={installApp}>
             <Download size={16} />
             Install <span className="hidden md:inline-block"> the app</span>
           </button>
