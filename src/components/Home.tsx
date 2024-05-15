7b779a275810737e76af39d65dbf88e1708a85db
Nick <njaginicholas@gmail.com>
2024-05-15 12:19:49 +0300

    update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,7 +9,8 @@ import { useAppSelector } from "../hooks";
 const Home = () => {
   const {currentMonth} = useAppSelector(state => state.date)
   return (
-    <section className="px-4 md:px-8 max-w-5xl mx-auto">
+    <section className="px-4 pt-6 md:px-8 max-w-5xl mx-auto">
+      
       <h1 className="text-center"><span className="inline-block mt-6 mb-2 text-7xl font-lato capitalize">{months[currentMonth]}</span></h1>
       <h1 className="font-semibold tracking-widest text-center mb-8">MONTHLY PLANNER <span className="inline-block font-semibold ">{new Date().getFullYear()}</span></h1>
 
