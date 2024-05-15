99da342f21a16d14714278ab679799a260fa5b14
Nick <njaginicholas@gmail.com>
2024-05-15 11:39:12 +0300

    add install button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import Achievements from "./components/achievements";
 import Home from "./components/Home";
+import InstallButton from "./components/InstallButton";
 import WeekView from "./components/WeekView";
 
 function App() {
   return (
     <main>
+      <InstallButton />
       <Home />
       <WeekView />
       <Achievements />
