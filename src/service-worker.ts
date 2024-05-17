ef316e572410aaeb2b3bed1736ba50646a3b7831
Nick <njaginicholas@gmail.com>
2024-05-17 11:31:32 +0300

    update service-worker.ts

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -36,17 +36,20 @@ registerRoute(
   ({ request, url }: { request: Request; url: URL }) => {
     // If this isn't a navigation, skip.
     if (request.mode !== 'navigate') {
+      console.log('Not a navigation request.');
       return false;
     }
 
     // If this is a URL that starts with /_, skip.
     if (url.pathname.startsWith('/_')) {
+      console.log('Path starts with /_.');
       return false;
     }
 
     // If this looks like a URL for a resource, because it contains
     // a file extension, skip.
-    if (url.pathname.match(fileExtensionRegexp)) {
+    if (fileExtensionRegexp.test(url.pathname)) {
+      console.log('Path matches file extension regexp.');
       return false;
     }
 
