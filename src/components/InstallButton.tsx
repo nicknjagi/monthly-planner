7c84a4dd9c16a60263ee6758af8de960625713ef
Nick <njaginicholas@gmail.com>
2024-05-17 12:37:04 +0300

    update InstallButton.tsx

--- src/components/InstallButton.tsx
+++ src/components/InstallButton.tsx
@@ -9,10 +9,6 @@ interface BeforeInstallPromptEvent extends Event {
   }>;
 }
 
-const isIphone = () => {
-  const platform = navigator.userAgent.toLowerCase();
-  return platform.includes('iphone') || platform.includes('ipad') 
-};
 
 const InstallButton: React.FC = () => {
   const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(null);
@@ -20,7 +16,7 @@ const InstallButton: React.FC = () => {
   const [showInstallButton, setShowInstallButton] = useState<boolean>(() =>{
     // Check if the app has been installed before
     const isAppInstalled = localStorage.getItem('appInstalled') === 'true';
-    return !isAppInstalled && !window.matchMedia('(display-mode: standalone)').matches && !isIphone()
+    return !isAppInstalled && !window.matchMedia('(display-mode: standalone)').matches 
   });
 
 
@@ -70,9 +66,11 @@ const InstallButton: React.FC = () => {
     }
   };
 
+  const isSafari = /^((?!chrome|android).)*safari/.test(navigator.userAgent.toLowerCase());
+
   return (
     <div className='flex justify-end absolute top-0 right-0'>
-      {showInstallButton  && (
+      {!isSafari && showInstallButton  && (
           <button className="flex gap-1 items-center px-4 py-2 rounded-full bg-forrest-green hover:bg-opacity-90 transition text-white m-2 shadow-md shadow-green-50" onClick={installApp}>
             <Download size={16} />
             Install <span className="hidden md:inline-block"> the app</span>
