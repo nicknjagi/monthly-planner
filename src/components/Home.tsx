898c9234ad9e9803b0e6f55f4e9d687fbcd86d32
Nick <njaginicholas@gmail.com>
2024-05-19 19:55:22 +0300

    update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,9 +10,26 @@ const Home = () => {
   const {currentMonth} = useAppSelector(state => state.date)
   return (
     <section className="px-4 py-6 md:px-8 max-w-5xl mx-auto">
-      
-      <h1 className="text-center"><span className="inline-block mt-6 mb-2 text-7xl font-lato capitalize">{months[currentMonth]}</span></h1>
-      <h1 className="font-semibold tracking-widest text-center mb-8">MONTHLY PLANNER <span className= "inline-block font-semibold ">{new Date().getFullYear()}</span></h1>
+      <div className="flex justify-between">
+        <img
+          className="w-28 md:w-40 h-fit aspect-[3/2] -translate-x-2 md:-translate-x-6"
+          src="./assets/images/logo.png"
+          alt="logo"
+        />
+        <div className="flex flex-col justify-end">
+          <h1 className="text-right">
+            <span className="inline-block mb-2 text-3xl md:text-5xl font-lato capitalize">
+              {months[currentMonth]}
+            </span>
+          </h1>
+          <h1 className="font-semibold tracking-widest text-right mb-8">
+            <span className="inline-block font-semibold ">
+              {new Date().getFullYear()}
+            </span>{" "}
+            <span className="block">DIGITAL PLANNER</span>{" "}
+          </h1>
+        </div>
+      </div>
 
       <div className="grid h-fit md:grid md:grid-cols-2 gap-8">
         <CustomCalender />
@@ -27,6 +44,6 @@ const Home = () => {
         </div>
       </section>
     </section>
-  )
+  );
 }
 export default Home
