4fbfc69aa637a831a6c4c3b4a797c894513010e4
Nick <njaginicholas@gmail.com>
2024-05-19 19:55:54 +0300

    update WeekView.tsx

--- src/components/WeekView.tsx
+++ src/components/WeekView.tsx
@@ -33,14 +33,28 @@ const WeekView = () => {
   }
 
   return (
-    <section className="w-full px-4 md:px-8 py-20 bg-white">
-      <div className="flex flex-col gap-8 max-w-[960px] mx-auto">
-        <div className="flex flex-col md:flex-row justify-between gap-4">
-          <div className="order-last md:order-first">
-            <h3 className="text-4xl lg:text-6xl font-medium capitalize mb-2">Week {currentWeek}</h3>
-            <div className="flex gap-2">
-              <button onClick={handlePrev} className="btn">Previous</button>
-              <button onClick={handleNext} className="btn">Next</button>
+    <section className="w-full px-4 md:px-8 pt-8 bg-white">
+      {/* <div className="max-w-[960px] mx-auto">
+        <img
+          className="w-28 md:w-40 h-fit aspect-[3/2] -translate-x-2 md:-translate-x-6 mb-8"
+          src="./assets/images/logo.png"
+          alt="logo"
+        />
+      </div> */}
+      <div className="flex flex-col gap-6 max-w-[960px] mx-auto">
+        <div className="flex flex-col justify-between mb-4">
+          <div className="flex justify-between md:items-center mb-4">
+            <img
+              className="w-28 md:w-40 h-fit aspect-[3/2] -translate-x-2 md:-translate-x-6"
+              src="./assets/images/logo.png"
+              alt="logo"
+            />
+            <div>
+              <h3 className="text-3xl md:text-5xl text-right capitalize mb-2">Week {currentWeek}</h3>
+              <div className="flex gap-2">
+                <button onClick={handlePrev} className="btn">Previous</button>
+                <button onClick={handleNext} className="btn">Next</button>
+              </div>
             </div>
           </div>
           <Months />
